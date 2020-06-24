b373cecc0ce77fba8add671643e809d5e965f9bb
Sher-Min Yang <sherminyang@gmail.com>
2020-06-23 23:29:28 -0400

    fix OneAway

--- src/Chapter1/1.5.OneAway.js
+++ src/Chapter1/1.5.OneAway.js
@@ -8,7 +8,7 @@ function oneAway(str1, str2) {
     for (let i = 0, j = 0; i < str1.length, j < str2.length; i++, j++) {
         if (str1[i] !== str2[j]) {
             differences++;
-            if (str1[i] !== str2[j] && str1.length !== str2.length) {
+            if (str1.length !== str2.length) {
                 if (str1[i + 1] === str2[j]) {
                      i++;
                 } else if (str1[i] === str2[j + 1] && str1.length !== str2.length)  {
